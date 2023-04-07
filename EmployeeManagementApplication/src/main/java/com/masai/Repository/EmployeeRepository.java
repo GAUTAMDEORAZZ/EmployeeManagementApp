778036ab025ab810b6537a0d5cf0445c5ed32a51
Gautam Deorazz <gdeoprasad@gmail.com>
2023-04-07 17:21:49 +0530

    add repository

--- EmployeeManagementApplication/src/main/java/com/masai/Repository/EmployeeRepository.java
+++ EmployeeManagementApplication/src/main/java/com/masai/Repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.masai.Repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.Model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
