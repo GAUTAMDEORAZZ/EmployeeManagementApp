10168b29d48f70d40c304eccc3addb292b4c99f0
Gautam Deorazz <gdeoprasad@gmail.com>
2023-04-07 18:01:30 +0530

    Add GlobalException handler

---
+++ EmployeeManagementApplication/src/main/java/com/masai/ExceptionHandler/DepartmentException.java
@@ -0,0 +1,12 @@
+package com.masai.ExceptionHandler;
+
+public class DepartmentException extends Exception{
+	public DepartmentException(String err) {
+		super(err);
+	}
+	
+	public DepartmentException() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ EmployeeManagementApplication/src/main/java/com/masai/ExceptionHandler/EmployeeException.java
@@ -0,0 +1,12 @@
+package com.masai.ExceptionHandler;
+
+public class EmployeeException extends Exception{
+	public EmployeeException(String err) {
+		super(err);
+	}
+	
+	public EmployeeException() {
+		
+	}
+
+}
