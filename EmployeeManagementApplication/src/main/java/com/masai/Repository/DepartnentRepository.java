9b8e480f1751e9e18a719df6b85b5d1c9890423c
Gautam Deorazz <gdeoprasad@gmail.com>
2023-04-07 17:21:12 +0530

    add Repository

---
+++ EmployeeManagementApplication/src/main/java/com/masai/Repository/DepartnentRepository.java
@@ -0,0 +1,9 @@
+package com.masai.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.Model.Department;
+
+public interface DepartnentRepository extends JpaRepository<Department, Integer> {
+
+}
